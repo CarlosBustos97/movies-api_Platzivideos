6d0ed3d4325bbad5d4dd337b6cf0765ebb03a116
Carlos Bustos <carlosbustos1997@gmail.com>
2021-08-25 12:40:02 -0500

    Se agrega boom para el manejo de errores y se añade middleware para error 404

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
@@ -11,9 +12,14 @@ app.use(express.json());
 //Ruta
 moviesApi(app);
 
+// captura error 404
+app.use(notFoundHandler);
+
 //middleware de errores --> deben ir al final de las rutas
 app.use(errorHandler);
+app.use(wrapErrors);
 app.use(logErrors);
+
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,8 +1,9 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function witchErrorStack(error, stack) {
   if (config.dev) {
-    return error, stack;
+    return {...error, stack};
   }
   return error;
 }
@@ -15,13 +16,22 @@ function logErrors(err, req, res, next) {// eslint-disable-line
   next(err);
 }
 
+function wrapErrors(err, req, res, next) {// eslint-disable-line
+  if(!err.isBoom){
+    next(boom.badImplementation(err));
+  }
+  next(err);
+}
+
 //Convierte la respuesta en JSON
 function errorHandler(err, req, res, next) { // eslint-disable-line
-  res.status(err.status || 500);
-  res.json(witchErrorStack(err.message, err.stack));
+  const { output: { statusCode, payload }} = err;
+  res.status(statusCode);
+  res.json(witchErrorStack(payload, err.stack));
 }
 
 module.exports = {
     logErrors,
+    wrapErrors,
     errorHandler
 };
