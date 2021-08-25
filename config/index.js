7d53bf1ef7825e463b2709828005f3dc19422ef0
Carlos Bustos <carlosbustos1997@gmail.com>
2021-08-25 16:52:46 -0500

    Se crean test para endpoint y servicios

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const config = {
-  // dev: process.env.NODE_ENV !== 'production',
-  dev: process.env.NODE_ENV.trim() !== 'production',
+const config = {  
+  //Para Pruebas
+  dev: process.env.NODE_ENV !== 'production',
+  // dev: process.env.NODE_ENV.trim() !== 'production',
   port: process.env.PORT || 3000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
