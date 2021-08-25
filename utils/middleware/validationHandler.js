2948788118b292c96f6c253d06aecfe2da281ee9
Carlos Bustos <carlosbustos1997@gmail.com>
2021-08-25 13:20:28 -0500

    Se corrigen errores en el validateHandler

--- utils/middleware/validationHandler.js
+++ utils/middleware/validationHandler.js
@@ -1,9 +1,10 @@
 const boom = require('@hapi/boom');
 const joi = require('@hapi/joi');
 
-function validate(data, schema) {
-  const { error } = joi.validate(data, schema);
-  return error;
+const validate = (data, schema) => {
+  const { error } = joi.object(schema).validate(data)
+
+  return error
 }
 
 function validationHandler(schema, check = 'body') {
